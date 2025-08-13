44af796b9ec46542a22389b3b92886b916d0dab7
Felipe Lima <flimalimafelipek@gmail.com>
2025-08-13 16:15:42 -0300

    fix: update in router delete

--- src/services/routes/events/delete-event.ts
+++ src/services/routes/events/delete-event.ts
@@ -2,7 +2,9 @@ import { api } from "@/config/axios"
 
 export const DeleteEvent = async (id: string) => {
   try {
-    const { data } = await api.delete(`/event/delete/${id}`)
+    const { data } = await api.delete(`/event/delete/${id}`, {
+      data: {"P": "A"}
+    })
     const event = data
     console.log("Excluido com sucesso!", event)
   } catch (error) {
