4e6c0fa884b165ff52471d3cf535129512192138
Felipe Lima <flimalimafelipek@gmail.com>
2025-08-23 10:01:00 -0300

    feat: update in section news

--- src/paginas/administrative/components/layouts/card-news.tsx
+++ src/paginas/administrative/components/layouts/card-news.tsx
@@ -23,7 +23,7 @@ export default function CardNews(data: CardNoticiasDTO) {
     <article className="flex h-[300px] w-[280px] flex-col justify-between rounded-[0.9rem] shadow-shadowCardEventLocation">
       {/* Container Foto  */}
       <div className="relative mb-1 h-[50%] w-full overflow-hidden rounded-tl-[0.9rem] rounded-tr-[0.9rem]">
-        <img src={data.photoURLs} className="h-full w-full object-cover" alt="Foto da Notícia" />
+        <img src={photo} className="h-full w-full object-cover" alt="Foto da Notícia" />
 
         {/* container de controles */}
         <div className="left-0-0 absolute bottom-0 flex w-full items-center justify-end gap-3 p-2">
--- src/paginas/administrative/components/layouts/modal/modal-update-news.tsx
+++ src/paginas/administrative/components/layouts/modal/modal-update-news.tsx
@@ -15,6 +15,7 @@ interface ModalUpdateNewsProps {
 export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
   // State utils in componente
   const [preview, setPreview] = useState<File | null>(null)
+  const [removePhoto, setRemovePhoto] = useState(false)
 
   const photo = props.data.photoURLs?.[0]?.url
     ? baseUrlPhoto('place', props.data.photoURLs[0].url) || backgroundloginpage
@@ -32,7 +33,15 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
     setPreview(null)
   }
 
-  function deletePhotoBackend() {}
+  function markPhotoForDeleteion() {
+    setRemovePhoto(true)
+  }
+
+  function handleVisibilityModal(){
+    setPreview(null)
+    setRemovePhoto(false)
+    props.handleVisibilityModalUpdate()
+  }
 
   return ReactDOM.createPortal(
     <section
@@ -48,7 +57,7 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
           <h1 className="text-lg font-semibold text-gray-800">Atualizar Notícia</h1>
           <div
             className="h-[30px] w-[30px] text-gray-600 hover:text-gray-800"
-            onClick={props.handleVisibilityModalUpdate}
+            onClick={handleVisibilityModal}
           >
             <IconClosed />
           </div>
@@ -63,7 +72,7 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
             </label>
 
             {preview ? (
-              //  Visualização da Nova Foto Adicionada
+              //  Visualização da Nova Foto Adicionado
               <div className="relative h-[200px] w-full overflow-hidden">
                 {props.data.photoURLs && (
                   <img
@@ -75,7 +84,28 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
                 <div className="absolute left-0 top-0 h-full w-full p-2">
                   <div className="flex w-full items-end justify-end">
                     <div
-                      onClick={preview ? deletePreviewPhoto : deletePhotoBackend}
+                      onClick={preview ? deletePreviewPhoto : markPhotoForDeleteion}
+                      className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-[5.97px] bg-white transition-all duration-500 hover:bg-primaryWhite500"
+                    >
+                      <IconTrash />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : !removePhoto && photo ? (
+              // renderiza Photo backend 
+               <div className="relative h-[200px] w-full overflow-hidden">
+                {props.data.photoURLs && (
+                  <img
+                    src={photo}
+                    className="h-full w-full rounded-[0.5rem] object-cover"
+                    alt=""
+                  />
+                )}
+                <div className="absolute left-0 top-0 h-full w-full p-2">
+                  <div className="flex w-full items-end justify-end">
+                    <div
+                      onClick={markPhotoForDeleteion}
                       className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-[5.97px] bg-white transition-all duration-500 hover:bg-primaryWhite500"
                     >
                       <IconTrash />
@@ -84,6 +114,7 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
                 </div>
               </div>
             ) : (
+              // Adicionar Nova Foto 
               <div className="relative flex h-48 w-full flex-col items-center justify-center gap-2 overflow-hidden rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 p-2 text-sm text-gray-500">
                 <input
                   type="file"
@@ -93,7 +124,6 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
                 />
                 <p>Escolha a Nova Foto da Notícia</p>
               </div>
-              // Visualização da Foto do BD
             )}
           </div>
 
@@ -111,10 +141,7 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
 
           {/* Update Content  */}
           <div className="flex w-full flex-col">
-            <label
-              htmlFor="content author"
-              className="block text-sm font-medium text-gray-700"
-            >
+            <label htmlFor="content author" className="block text-sm font-medium text-gray-700">
               Conteúdo da Notícia
             </label>
             <textarea
@@ -129,11 +156,17 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
             <label className="mb-1 block text-sm font-medium text-gray-700" htmlFor="name author">
               Nome do Autor
             </label>
-            <input type="text" className="w-full rounded border border-gray-300 p-2 text-sm" placeholder='Digite o nome do Author' />
+            <input
+              type="text"
+              className="w-full rounded border border-gray-300 p-2 text-sm"
+              placeholder="Digite o nome do Author"
+            />
           </div>
 
           {/* button of submited  */}
-          <button className="w-full rounded bg-primargreen py-2 text-white">Atualizar Notícia</button>
+          <button className="w-full rounded bg-primargreen py-2 text-white">
+            Atualizar Notícia
+          </button>
         </form>
       </article>
     </section>,
