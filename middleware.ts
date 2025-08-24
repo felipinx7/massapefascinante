ad2afa8866b9558a5a0e6a79b2eb885366982f2a
Felipe Lima <flimalimafelipek@gmail.com>
2025-08-24 11:39:58 -0300

    feat: impletation of protect router

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export default function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')
+
+  if (!token && request.nextUrl.pathname.startsWith('/administrative')) {
+    return NextResponse.redirect(new URL('/login-page', request.url))
+  }
+
+  return NextResponse.next()
+}
