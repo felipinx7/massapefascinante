95d55b158da41f7519cb9832694e7efcea702deb
Felipe Lima <flimalimafelipek@gmail.com>
2025-08-24 11:57:58 -0300

    fix: update in protect router

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,12 @@ import { NextRequest, NextResponse } from 'next/server'
 export default function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
 
+  console.log('Middleware executado!')
+  console.log('Path:', request.nextUrl.pathname)
+  console.log('Token:', token)
+
   if (!token && request.nextUrl.pathname.startsWith('/administrative')) {
+    console.log('Redirecionando para login-page')
     return NextResponse.redirect(new URL('/login-page', request.url))
   }
 
