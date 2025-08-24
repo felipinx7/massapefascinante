71f6183797d5b9df95b14392b50ad11f4864b62c
Felipe Lima <flimalimafelipek@gmail.com>
2025-08-24 11:49:40 -0300

    feat: impletation of protect router

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export default function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.name
+  const token = request.cookies.get('token')?.value
 
   if (!token && request.nextUrl.pathname.startsWith('/administrative')) {
     return NextResponse.redirect(new URL('/login-page', request.url))
   }
 
   return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/administrative/:path*'],
 }
