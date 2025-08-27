ce79a7aa268b1470f63e77801337afa9b7465722
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-08-27 16:23:27 -0300

    fix: News components correcctions

--- src/paginas/noticias/components/card-noticias-relevantes.tsx
+++ src/paginas/noticias/components/card-noticias-relevantes.tsx
@@ -7,17 +7,12 @@ export default function CardNoticiasRelevantes(data: CardNoticiasDTO) {
     ? baseUrlPhoto('news', data.photo[0].url) || backgroundloginpage
     : backgroundloginpage
   return (
-    <article className="group relative cursor-pointer flex items-start justify-start gap-2 max-lg:flex-col max-lg:justify-between">
-      <div className="h-[100px] overflow-clip w-[250px] rounded-[5.97px] max-lg:h-auto">
-        <img
-          src={photo}
-          className="max-h-[100px] group-hover:scale-125 duration-500 transition-all max-w-[250px] rounded-lg object-cover ease-in-out hover:scale-105 max-lg:max-h-none max-lg:w-full max-lg:max-w-none"
-          alt=""
-        />
-      </div>
-      <div className="flex flex-col">
-        <h1 className="line-clamp-3 font-bold">{data.title}</h1>
-      </div>
+    <article className="w-72 flex h-18 cursor-pointer items-start gap-2 transition-all duration-300 ease-in-out hover:scale-105">
+        <div className="w-32 h-18 flex" style={{backgroundImage: `url(${photo})`, backgroundSize: "cover"}}></div>
+        <div className="flex flex-col w-full h-full">
+        <h1 className="line-clamp-2 text-slate-950 font-normal">{data.title}</h1>
+        <h1 className="line-clamp-2 text-slate-950 font-normal text-sm">{data.author}</h1>
+        </div>
     </article>
   )
 }
