90e689f2e4a279d5ebd8a76ceced96a6d2215677
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-08-28 14:37:24 -0300

    .

--- src/paginas/noticiaSingle/components/card-mais-noticias.tsx
+++ src/paginas/noticiaSingle/components/card-mais-noticias.tsx
@@ -1,20 +1,20 @@
+import { backgroundloginpage } from "@/assets/image"
 import { CardNoticiasDTO } from "@/dto/news/DTO-news"
+import { baseUrlPhoto } from "@/utils/base-url-photos"
 
-
-export default function CardMaisNoticias(data: CardNoticiasDTO) {
+export default function CardNoticiasRelevantes(data: CardNoticiasDTO) {
+    const photo = data.photo?.[0]?.url
+    ? baseUrlPhoto('news', data.photo[0].url) || backgroundloginpage
+    : backgroundloginpage
   return (
-    <article className="group relative flex w-full cursor-pointer items-start gap-2  transition-all duration-500 ease-in-out hover:scale-105 flex-col justify-between">
-      <div className="h-full w-full overflow-clip rounded-[5.97px]">
-        <img
-          src={data.photoURLs[0].url}
-          className="h-full w-full rounded-[5.97px] object-cover transition-all duration-500 ease-in-out group-hover:scale-125"
-          alt=""
-        />
-      </div>
-      <div className="flex flex-col">
-        <p className="text-[0.9rem]">{data.date}</p>
-        <h1 className="line-clamp-3 font-bold">{data.title}</h1>
-      </div>
+    <article className=" flex z-100  h-72 w-56  cursor-pointer flex-col items-start gap-2 transition-all duration-300 ease-in-out hover:scale-105">
+        <div className="w-56 h-56 flex rounded-lg" style={{backgroundImage: `url(${photo})`, backgroundSize: "cover"}}></div>
+        <div className="flex flex-col w-56 items-start h-full">
+        <h1 className="line-clamp-3 text-slate-950 text-sm font-normal">{data.title}</h1>
+        <h1 className="line-clamp-1 text-slate-950 font-normal text-xs">{data.date}</h1>
+        <h1 className="line-clamp-1 text-slate-950 font-normal text-xs">{data.author}</h1>
+        </div>
     </article>
   )
+  
 }
