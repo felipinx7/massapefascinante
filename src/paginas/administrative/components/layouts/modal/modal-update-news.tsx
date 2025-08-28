dc9d524429d2f20670bdbc0ad842044ebfc38d5e
Felipe Lima <flimalimafelipek@gmail.com>
2025-08-28 15:14:33 -0300

    feat: update in page

--- src/paginas/administrative/components/layouts/modal/modal-update-news.tsx
+++ src/paginas/administrative/components/layouts/modal/modal-update-news.tsx
@@ -74,7 +74,8 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
           console.error('Não existe ID/URL da foto antiga para atualizar')
         } else {
           const responsePhoto = await updatePhoto(props.data.photo[0].url, photoFile)
-          console.log('Foto atualizada com sucesso:', responsePhoto?.data)
+          console.log('Tentando atualizar foto com:', props.data.photo[0].url, photoFile)
+          return responsePhoto
         }
       }
 
@@ -87,7 +88,7 @@ export default function ModalUpdateNews(props: ModalUpdateNewsProps) {
         photoURLs: removePhoto ? [] : (data.photoURLs ?? []),
       }
 
-      const responseNews = await updateNews(payload)
+      const responseNews = await updateNews(payload, props.data.id)
       console.log('Notícia atualizada com sucesso:', responseNews?.data ?? 'sem body de resposta')
 
       reset()
