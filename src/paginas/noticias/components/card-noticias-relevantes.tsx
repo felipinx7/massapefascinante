2364f461c646ec1114b7c5a86a579a07f9273cd6
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-09-01 15:45:23 -0300

    .

--- src/paginas/noticias/components/card-noticias-relevantes.tsx
+++ src/paginas/noticias/components/card-noticias-relevantes.tsx
@@ -9,7 +9,7 @@ export default function CardNoticiasRelevantes(data: CardNoticiasDTO) {
     : backgroundloginpage
   return (
     <article className="w-[400px] flex z-100  max-lg:h-72 max-lg:w-44 h-28 cursor-pointer max-lg:flex-col items-start gap-2 transition-all duration-300 ease-in-out hover:scale-105">
-        <img src={photo} className="w-44 max-lg:w-44 max-lg:h-48 h-28 flex rounded-lg" alt="" />
+        <img src={photo} className="w-44 object-cover max-lg:w-44 max-lg:h-48 h-28 flex rounded-lg" alt="" />
         <div className="flex flex-col w-36 items-start h-full">
         <h1 className="line-clamp-3 text-slate-950 text-sm font-normal">{data.title}</h1>
         <h1 className="line-clamp-1 text-slate-950 font-normal text-xs">{formatData(data.date)}</h1>
