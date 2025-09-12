1861ab2fb51af5b710f31e975648348719e5158c
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-09-12 15:16:45 -0300

    .

--- src/paginas/taxi-drivers/components/card-taxi.tsx
+++ src/paginas/taxi-drivers/components/card-taxi.tsx
@@ -15,11 +15,11 @@ export const CardTaxi = (props: dataInfoTaxi) => {
   return (
     <article className="flex w-[280px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition hover:shadow-xl max-md:w-full">
       {/* Imagem com fundo suave, ocupando bem o espaço */}
-      <div className="relative h-[180px] w-full bg-gradient-to-tr from-[#f0f4ff] to-[#dbe8ff]">
+      <div className="relative h-[180px] max-h-[180px] w-full bg-gradient-to-tr from-[#f0f4ff] to-[#dbe8ff]">
         <img
           src={photoBaseUrl || backgroundclientpage}
           alt="Foto do taxista"
-          className="rounded-t-xl object-cover"
+          className="rounded-t-xl w-full h-full object-cover"
         />
       </div>
 
