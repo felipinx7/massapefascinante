37eae0ac7838e036998d8234bd110500867f1b19
ChHenrique <ch1claudiohenrique@gmail.com>
2025-09-15 20:17:18 -0300

    ambient prod

--- src/utils/base-url-photo.ts
+++ src/utils/base-url-photo.ts
@@ -1,12 +1,14 @@
 import { backgroundloginpage } from "@/assets/image";
 import { CardEventPageAdministrative } from "@/dto/event/data-card-event-DTO";
 
-export function BaseUrl (photoURL: string, props: CardEventPageAdministrative){
-     const photoUrl =
+// Pega a URL base do backend do env
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export function BaseUrl(photoURL: string, props: CardEventPageAdministrative) {
+  const photoUrl =
     props.photos.length > 0
-      ? `https://api.massapefascinante.com.br/${photoURL}`
-      : backgroundloginpage
+      ? `${API_URL}/${photoURL}`  // usa a URL do env
+      : backgroundloginpage;
 
-      return photoUrl
-
+  return photoUrl;
 }
--- src/utils/base-url-photos.ts
+++ src/utils/base-url-photos.ts
@@ -1,5 +1,7 @@
 export const BASE_URL_BACK_END = 'https://api.massapefascinante.com.br/uploads/'
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export function baseUrlPhoto(
   type: 'city' | 'event' | 'place' | 'taxiDrivers' | 'room' | 'news',
   photoName: string | undefined | null,
@@ -9,5 +11,5 @@ export function baseUrlPhoto(
   // Se já for uma URL (começa com http), apenas retorna como está
   if (photoName.startsWith('http')) return photoName
 
-  return `${BASE_URL_BACK_END}${type}/${photoName}`
+  return `${API_URL}/${type}/${photoName}`
 }
