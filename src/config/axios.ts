3797891cef3e294ad3d991cd3c15c89627413bb4
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-09-16 14:39:29 -0300

    .

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 // Configure axios instance, with baseURL, that will be used for all requests and withCredentials set to true
 export const api = axios.create({
-    baseURL: `${API_URL}/`,
+    baseURL: `https://www.massapefascinante.com.br/`,
     withCredentials: true,
 });
